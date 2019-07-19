981a1e9bce6c2454863f942439f8896a9bb6bad8
严瑞 <2221595915@qq.com>
2019-07-19 17:59:57 +0800

    update

--- yan-video-dev/yanVideo-mini-api/src/main/java/com/yr/WebMvcConfig.java
+++ yan-video-dev/yanVideo-mini-api/src/main/java/com/yr/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/").addResourceLocations("file:E:/yan-video-file/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/")
+                .addResourceLocations("file:D:/yan-video-file/");
     }
 }
--- yan-video-dev/yanVideo-mini-api/src/main/java/com/yr/controller/BasicControll.java
+++ yan-video-dev/yanVideo-mini-api/src/main/java/com/yr/controller/BasicControll.java
@@ -1,10 +1,12 @@
 package com.yr.controller;
 
 import com.yr.utils.RedisOperator;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "基础静态字符")
 public class BasicControll {
 
     @Autowired
@@ -12,10 +14,11 @@ public class BasicControll {
 
     public static final String USER_REDIS_SESSION = "uesr-redis-session";
     // 文件保存的命名空间
-    public static final String FILE_SPACE = "E:/yan-video-file";
+    public static final String FILE_SPACE = "D:/yan-video-file";
 
     //ffmpge
     public static final String FFMPEG_EXE = "D:\\ffmpeg\\bin\\ffmpeg.exe";
 
+    //每页显示个数
     public static final Integer PAGE_SIZE = 5;
 }
